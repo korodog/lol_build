dac52001f604c9f03aa4a4483f5f66310fa896ae
korodog <korodog56@gmail.com>
2021-10-14 01:43:15 +0900

    list-model

--- lib/build_list/build_list_model.dart
+++ lib/build_list/build_list_model.dart
@@ -3,20 +3,21 @@ import 'package:flutter/material.dart';
 import 'package:lol_build/domain/build.dart';
 
 class BuildListModel extends ChangeNotifier {
-  final Stream<QuerySnapshot> _usersStream =
-  FirebaseFirestore.instance.collection('builds').snapshots();
+
   List<Build>? builds;
 
-  void fetchBuildList() {
-    _usersStream.listen((QuerySnapshot snapshot) {
-      final List<Build> builds = snapshot.docs.map((DocumentSnapshot document) {
-        Map<String, dynamic> data = document.data() as Map<String, dynamic>;
-        final String title = data['title'];
-        final String memo = data['memo'];
-        return Build(title, memo);
-      }).toList();
+  void fetchBuildList() async{
+    final QuerySnapshot snapshot =
+        await FirebaseFirestore.instance.collection('builds').get();
+
+    final List<Build> builds = snapshot.docs.map((DocumentSnapshot document) {
+      Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+      final String title = data['title'];
+      final String memo = data['memo'];
+      return Build(title, memo);
+    }).toList();
+
       this.builds = builds;
       notifyListeners();
-    });
   }
 }
